09d81f31784e709410fd49c8d63bb5123c6c7dc3
Your Name <you@example.com>
2018-10-29 18:24:45 +0100

    segona modificacio

--- ldapserver:18practica/install.sh
+++ ldapserver:18practica/install.sh
@@ -6,7 +6,7 @@ rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 cp /opt/docker/DB_CONFIG /var/lib/ldap
 slaptest -F /etc/openldap/slapd.d -f /opt/docker/slapd.conf
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.ldif
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/prac.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /var/lib/ldap
 
