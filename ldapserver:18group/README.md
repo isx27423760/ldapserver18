aa9191b268223d2b29cb78212b205786d1e84834
isx27423760 <isx27423760@i16.informatica.escoladeltreball.org>
2018-11-15 09:54:33 +0100

    pa

---
+++ ldapserver:18group/README.md
@@ -0,0 +1,6 @@
+# ldapserver:18group
+
+## @edt ASIX M06-AS = curs 2018-2019
+
+Servidor ldap amb edt.org totes les dades per uid i es
+conectara amb el client que configurem amb pam
---
+++ ldapserver:18group/startup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# @edt ASIX M06 2018-2019
+# crear i engegar slapd mb edt.org
+#--------------------------------------
+
+/opt/docker/install.sh && echo "Install OK"
+/bin/bash
+#/sbin/slapd && echo "slapd OK"
